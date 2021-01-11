b38e782e2ec06c12198b866c40a6cfc570e137e7
csvt99bg <52644878+csvt99bg@users.noreply.github.com>
2021-01-11 16:34:23 +0700

    update

--- app/src/main/java/com/example/longnt45/MainActivity.kt
+++ app/src/main/java/com/example/longnt45/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.longnt45
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnClick.setOnClickListener(View.OnClickListener {
+            Toast.makeText(baseContext,"Xin chao",Toast.LENGTH_SHORT).show()
+        })
     }
 }
